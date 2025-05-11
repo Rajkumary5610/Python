ac6cf7fb48d89a2abb96d7a1cd8f412f8eb4faa1
Rajkumary5610 <rajkumary5610@gmail.com>
2025-05-11 15:42:22 +0530

    First commit

---
+++ Interest_Caluclator.py
@@ -0,0 +1,33 @@
+
+#Python  Compound Interest Caluclator
+
+principle = 0
+rate = 0
+time = 0
+
+while True:
+    principle = float(input("Enter the principle amount: $"))
+    if principle < 0:
+        print("principle amount can't be lesser than  zero")
+    else: 
+        break
+
+while True:
+    rate  = float(input("Enter the interest rate: "))
+    if rate < 0:
+        print("interest rate can't be lesser than  zero")
+    else:
+        break
+
+while True:
+    time = int(input("Enter the time in year/s: "))
+    if time < 0:
+        print("time can't be lesser than zero")
+    else:
+        break
+    
+
+
+total = principle * pow(1 + rate / 100,time)
+
+print(f"Balance after {time} year/s : ${total:,.2f}")
