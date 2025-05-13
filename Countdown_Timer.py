22143297a16ca18d4a7bb809eda056c231dabe71
Rajkumary5610 <rajkumary5610@gmail.com>
2025-05-13 12:25:22 +0530

    First commit

---
+++ Countdown_Timer.py
@@ -0,0 +1,21 @@
+# countdown time
+
+import time         #importing time module
+
+my_time = int(input("Enter the time in seconds: "))     #Asking input from user
+
+
+#Computing time and displaying in Hours, minutes and seconds
+
+for x in range(my_time, 0, -1):
+    seconds = x % 60
+    minutes = int(x / 60) % 60
+    hours = int(x / 3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+    
+
+
+
+print("TIME UP")
+
